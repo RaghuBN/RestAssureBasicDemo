09104a7f6dd13e24bd1a0ad8c305125a3488e68d
RaghuBN <raghu.astepahead@gmail.com>
2019-11-01 23:44:39 +0530

    Getting Response from Server

--- RestAssuredByMaven/src/test/java/RestAssured/BasicFeatures.java
+++ RestAssuredByMaven/src/test/java/RestAssured/BasicFeatures.java
@@ -1,5 +1,16 @@
 package RestAssured;
 
-public class BasicFeatures {
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
 
+public class BasicFeatures {
+	@Test
+	public void testlogging() {
+		given().
+		get("https://reqres.in/api/users?page=2").
+		then().
+		statusCode(200).
+		log().all();
+		
+	}
 }
