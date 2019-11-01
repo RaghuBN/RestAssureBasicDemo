0d4ae3ade563f7fb8c73df0a661759a0a62c249a
RaghuBN <raghu.astepahead@gmail.com>
2019-11-01 23:46:30 +0530

    testequalToFuntion

--- RestAssuredByMaven/src/test/java/RestAssured/BasicFeatures.java
+++ RestAssuredByMaven/src/test/java/RestAssured/BasicFeatures.java
@@ -2,6 +2,8 @@ package RestAssured;
 
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.*;
+import io.restassured.matcher.ResponseAwareMatcher;
+import io.restassured.response.Response;
 
 public class BasicFeatures {
 	@Test
@@ -13,4 +15,20 @@ public class BasicFeatures {
 		log().all();
 		
 	}
+	
+	@Test
+	public void testequalToFuntion() {
+		given().
+		get("https://reqres.in/api/users/2").
+		then().
+		body("data.id.first_name", equalTO("Janet"));
+		
+		
+	}
+
+	private ResponseAwareMatcher<Response> equalTO(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 }
